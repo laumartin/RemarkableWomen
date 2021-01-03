79d733918f79c48c88fe6d1d072536c67a60fb60
laumartin <lauratemar@gmail.com>
2021-01-03 15:07:20 +0000

    creating a new template file admin_manage.html to display and later edit by admin user some of the form field options. Connect the application for this first by creating the get_categories function in app.py and updating the ink on base.html using flask so only admin can see the admin menu in nav bar

--- app.py
+++ app.py
@@ -207,6 +207,12 @@ def delete_character(character_id):
     return redirect(url_for("characters"))
 
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("admin_manage.html", categories=categories)
+
+
 @app.route("/statistics")
 def statistics():
 

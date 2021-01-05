6723a3a6225ac9d9a295ce8416e44a0cfd78edd6
laumartin <lauratemar@gmail.com>
2021-01-05 13:58:22 +0000

    changing characters.html page layout grid structure, adding toggler for each card to diplay more content on click through jquery in script.js. Amending search functionality for the no results case in app.py and characters.html. Amending cancel button in edit characters.html

--- static/script.js
+++ static/script.js
@@ -1,17 +1,8 @@
 
-// To initialise the bootstrap library ekkolightbox, using event listener on the <a>tags with data-toggle=lightbox 
-// to prevent the default behaviour (navigate to an URL). Then it initializes the lightbox.
-$(document).on("click", '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
-
-// To use the select country bootstrap package 
-$('.countrypicker').countrypicker();
 
 // Characters page see more details of each card toggle
-$("linktext").click(function(){
-    $("fulltext").toggle();
+$(".linktext").click(function(){
+    $(".fulltext").toggle();
 });
 
 
